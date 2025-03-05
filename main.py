d5b7c6163b3fab29ecc71aa76201ad15bbac0523
sickdawg5440 <kjkuhner@gmail.com>
2025-03-05 05:30:45 -0800

    added circleshape and FPS control

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import pygame # type: ignore
+import pygame
 from constants import *
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -13,7 +15,7 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000 
 
 if __name__ == "__main__":
     main()
